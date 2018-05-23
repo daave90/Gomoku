9f765f669c59973a5e92204e0544c2f524b83263
Dawid Maciak <dave.90@wp.pl>
2018-05-23 11:53:03 +0200

    Create human vs computer game

--- src/main/java/gui/GameController.java
+++ src/main/java/gui/GameController.java
@@ -112,61 +112,70 @@ public class GameController {
             computerVsComputer((ComputerPlayer) first, (ComputerPlayer) second);
         }
         else if(first instanceof HumanPalyer && second instanceof HumanPalyer){
-            humanVsHuman(first, second);
+            humanVsHuman();
         }
         else if((first instanceof HumanPalyer && second instanceof ComputerPlayer) ||
                 (first instanceof ComputerPlayer && second instanceof HumanPalyer)){
+            if(first instanceof ComputerPlayer){
+
+                ((ComputerPlayer) first).firstMove(logicBoard);
+                parseBoard(logicBoard);
+
+                //zmień obrazek na następnego gracza
+                if(logicBoard.getNextPlayer().getId() == Const.FIRST_PLAYER_ID){
+                    nextPlayerImage.setImage(Const.FIRST_PLAYER_IMAGE);
+                }
+                else {
+                    nextPlayerImage.setImage(Const.SECOND_PLAYER_IMAGE);
+                }
+
+            }
             computerVsHuman(first, second);
         }
     }
 
     private void computerVsComputer(ComputerPlayer first, ComputerPlayer second){
-        long start = System.currentTimeMillis();
-        first.firstMove(logicBoard);
-        while(!WinnerFinder.isWinner(logicBoard, first) &&
-                !WinnerFinder.isWinner(logicBoard, second) &&
+
+        //long start = System.currentTimeMillis();
+        ComputerPlayer computerPlayer = (ComputerPlayer) logicBoard.getNextPlayer();
+        computerPlayer.firstMove(logicBoard);
+        parseBoard(logicBoard);
+        changePicture();
+
+        while(!WinnerFinder.isWinner(logicBoard, logicBoard.getPrevPlayer()) &&
+                !WinnerFinder.isWinner(logicBoard, logicBoard.getNextPlayer()) &&
                 !logicBoard.getEmpties().isEmpty()){
-            first.makeMove(logicBoard);
+
+            computerPlayer = (ComputerPlayer) logicBoard.getNextPlayer();
+            computerPlayer.makeMove(logicBoard);
             parseBoard(logicBoard);
-            if(WinnerFinder.isWinner(logicBoard, first)){
-                if(first.getId() == 1){
-                    //DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły");
-                    System.out.println("Białe zwyciężyły " + first.getId());
-                }
-                else{
-                    //DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły");
-                    System.out.println("Czarne zwyciężyły " + first.getId());
-
-                }
-                break;
-            }
-
-            second.makeMove(logicBoard);
-            parseBoard(logicBoard);
-            if(WinnerFinder.isWinner(logicBoard, second)){
-                if(second.getId() == 1){
-                    //DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły!!!");
-                    System.out.println("Białe zwyciężyły " + second.getId());
-
-                }
-                else{
-                    //DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły!!!");
-                    System.out.println("Czarne zwyciężyły " + second.getId());
-
-                }
-                break;
-            }
+            changePicture();
         }
 
-        if(logicBoard.getEmpties().isEmpty()){
-            //DialogUtils.getInformationDialog("Koniec gry", "Rozgrywka zakończona remisem!!!");
-            System.out.println("Remis");
+        if(WinnerFinder.isWinner(logicBoard, logicBoard.getPrevPlayer())){
+            if(logicBoard.getPrevPlayer().getId() == Const.FIRST_PLAYER_ID){
+                DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły!!!");
+            }
+            else{
+                DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły!!!");
+            }
         }
-        long stop = System.currentTimeMillis();
-        System.out.println(stop - start);
+        else if(WinnerFinder.isWinner(logicBoard, logicBoard.getNextPlayer())){
+            if(logicBoard.getNextPlayer().getId() == Const.FIRST_PLAYER_ID){
+                DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły!!!");
+            }
+            else{
+                DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły!!!");
+            }
+        }
+        else if(logicBoard.getEmpties().isEmpty()){
+            DialogUtils.getInformationDialog("Koniec gry", "Rozgrywka zakończona remisem!!!");
+        }
+        //long stop = System.currentTimeMillis();
+        //System.out.println(stop - start);
     }
 
-    private void humanVsHuman(Player first, Player second){
+    private void humanVsHuman(){
         for(int i = 0; i < guiBoard.length; i++){
             for(int j = 0; j < guiBoard[i].length; j++){
                 guiBoard[i][j].setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -188,13 +197,9 @@ public class GameController {
                                     //wykonaj ruch na planszy logicznej
                                     logicBoard.setMove(i, j, logicBoard.getNextPlayer());
 
-                                    //zmień obrazek na następnego gracza
-                                    if(logicBoard.getNextPlayer().getId() == Const.FIRST_PLAYER_ID){
-                                        nextPlayerImage.setImage(Const.FIRST_PLAYER_IMAGE);
-                                    }
-                                    else{
-                                        nextPlayerImage.setImage(Const.SECOND_PLAYER_IMAGE);
-                                    }
+                                    //zmień obrazek następnego gracza
+                                    changePicture();
+
                                     break;
                                 }
                             }
@@ -213,11 +218,96 @@ public class GameController {
                             disableImageViews();
                         }
                     }
+                });
+            }
+        }
+    }
 
-                    private void disableImageViews(){
+    private void computerVsHuman(Player first, Player second){
+        for(int i = 0; i < guiBoard.length; i++){
+            for(int j = 0; j < guiBoard[i].length; j++) {
+                guiBoard[i][j].setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        //Gracz komputerowy musi być zawsze drugi ponieważ
+                        //To gracz człowiek klikając na pole wywołuje również algorytm min - max
+                        if(first instanceof ComputerPlayer){
+                            newRound(second, first, event);
+                        }
+                        else{
+                            newRound(first, second, event);
+                        }
+                    }
+
+                    private void newRound(Player first, Player second, MouseEvent event){
+
+                        //Znalezienie klikniętego pola
                         for(int i = 0; i < guiBoard.length; i++){
-                            for(int j = 0;  j < guiBoard[i].length; j++){
-                                guiBoard[i][j].setDisable(true);
+                            for(int j = 0; j < guiBoard[i].length; j++){
+
+                                //jeżeli pole zostało kliknięte i jest puste
+                                if(guiBoard[i][j] == event.getSource() &&
+                                        guiBoard[i][j].getImage() == Const.EMPTY_FIELD_IMAGE){
+
+                                    //zmień obrazek na gracza wykonującego ruch
+                                    guiBoard[i][j].setImage(nextPlayerImage.getImage());
+
+                                    //wykonaj ruch na planszy logicznej
+                                    logicBoard.setMove(i, j, logicBoard.getNextPlayer());
+
+                                    //zmień obrazek na następnego gracza
+                                    changePicture();
+
+                                    //Sprawdzenie zwycięstwa poprzedniego gracza lub remisu
+                                    if(WinnerFinder.isWinner(logicBoard, logicBoard.getPrevPlayer())){
+                                        if(logicBoard.getPrevPlayer().getId() == Const.FIRST_PLAYER_ID){
+                                            DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły!!!");
+                                        }
+                                        else{
+                                            DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły!!!");
+                                        }
+                                        disableImageViews();
+                                        break;
+                                    }
+                                    else if(logicBoard.getEmpties().isEmpty()){
+                                        DialogUtils.getInformationDialog("Koniec gry", "Rozgrywka zakończona remisem!!!");
+                                        disableImageViews();
+                                        break;
+                                    }
+
+                                    //Wyłączenie planszy na czas trwania ruchu komputera
+                                    disableImageViews();
+
+                                    //Ruch komputera
+                                    second.makeMove(logicBoard);
+                                    parseBoard(logicBoard);
+
+                                    //Sprawdzenie zwycięstwa poprzedniego gracza lub remisu
+                                    if(WinnerFinder.isWinner(logicBoard, logicBoard.getPrevPlayer())){
+                                        if(logicBoard.getPrevPlayer().getId() == Const.FIRST_PLAYER_ID){
+                                            DialogUtils.getInformationDialog("Koniec gry", "Białe zwyciężyły!!!");
+                                        }
+                                        else{
+                                            DialogUtils.getInformationDialog("Koniec gry", "Czarne zwyciężyły!!!");
+                                        }
+                                        disableImageViews();
+                                        break;
+                                    }
+                                    else if(logicBoard.getEmpties().isEmpty()){
+                                        DialogUtils.getInformationDialog("Koniec gry", "Rozgrywka zakończona remisem!!!");
+                                        disableImageViews();
+                                        break;
+                                    }
+
+                                    //zmień obrazek na następnego gracza
+                                    changePicture();
+
+                                    //Włączenie planszy
+                                    enableImageViews();
+                                    break;
+                                }
+
                             }
                         }
                     }
@@ -226,8 +316,32 @@ public class GameController {
         }
     }
 
-
-    private void computerVsHuman(Player first, Player second){
-
+    /**
+     * Funkcja zmienia obrazek w imageView
+     */
+    private void changePicture(){
+        if(logicBoard.getNextPlayer().getId() == Const.FIRST_PLAYER_ID){
+            nextPlayerImage.setImage(Const.FIRST_PLAYER_IMAGE);
+        }
+        else{
+            nextPlayerImage.setImage(Const.SECOND_PLAYER_IMAGE);
+        }
     }
+
+    private void disableImageViews(){
+        for(int i = 0; i < guiBoard.length; i++){
+            for(int j = 0;  j < guiBoard[i].length; j++){
+                guiBoard[i][j].setDisable(true);
+            }
+        }
+    }
+
+    private void enableImageViews(){
+        for(int i = 0; i < guiBoard.length; i++){
+            for(int j = 0;  j < guiBoard[i].length; j++){
+                guiBoard[i][j].setDisable(false);
+            }
+        }
+    }
+
 }
